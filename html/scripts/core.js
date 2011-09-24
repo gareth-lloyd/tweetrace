b0e2fa49e87e4ff8624a091411eaa59141effd03
Matthew Lane <lanoid@yahoo.com>
2011-09-24 17:03:40 +0100

    /html Folder Added
    Adding a basic interface with barebones js/css/html

---
+++ html/scripts/core.js
@@ -0,0 +1,17 @@
+define(['jquery'], function($){
+    function init(){
+        
+    }
+    
+    core = {
+        ajax : function(url, params){
+            
+        }
+    }
+    
+    $(function(){
+        init();
+    });
+    
+    return core;
+});
