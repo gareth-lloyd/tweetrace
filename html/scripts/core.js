2618d0ad22d1d98bd723bf3975ab1b40969b33eb
Matthew Lane <lanoid@yahoo.com>
2011-09-24 18:10:40 +0100

    Addition of mustache
    Mustache js added js functions stubbed out in preparation to receive
    data.

--- html/scripts/core.js
+++ html/scripts/core.js
@@ -4,8 +4,22 @@ define(['jquery'], function($){
     }
     
     core = {
-        ajax : function(url, params){
-            
+        ajax : function(url, params, callback){
+            $.get(
+                url,
+                params,
+                core.ajaxSuccess
+            );
+        },
+        
+        ajaxSuccess : function(data, textStatus){
+            if(textStatus !== 'ok'){
+                console.log(textStatus);
+            } else if(!calllback){
+                return data;
+            } else {
+                callback(data);
+            }
         }
     }
     
---
+++ html/scripts/templates.js
@@ -0,0 +1,11 @@
+define(function(){
+    templates = {
+        'twitter-block' :
+        '<article>'+
+            '<img src="{{src}}" height="80px" width="80px" />'+
+            '<p>{{message}}</p>'+
+        '</article>'
+    }
+    
+    return templates;
+});
